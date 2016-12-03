3f7812c99ff47fbb89b83bb1f3510da023e863fa
Yodaii <yohann.vaubourg@gmail.com>
2016-12-03 20:49:27 +0100

    séparation agent cyclic behaviour

--- src/main/java/agents/Receiver.java
+++ src/main/java/agents/Receiver.java
@@ -9,17 +9,6 @@ public class Receiver extends Agent
 {
     protected void setup() 
     {
-        addBehaviour(new CyclicBehaviour(this) 
-        {
-             public void action() 
-             {
-                ACLMessage msg= receive();
-                if (msg!=null)
-                    System.out.println( " - " +
-                       myAgent.getLocalName() + " <- " +
-                       msg.getContent() );
-                block();
-             }
-        });
+        addBehaviour(new ReceptionMessage(this));
     }
 }
---
+++ src/main/java/agents/ReceptionMessage.java
@@ -0,0 +1,35 @@
+package agents;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+import fr.miage.agents.api.message.Message;
+
+public class ReceptionMessage extends CyclicBehaviour{
+	Agent agent;
+	
+	public ReceptionMessage(Agent a){
+		agent = a;
+	}
+	
+	public void action() 
+    {
+       ACLMessage msg= agent.receive();
+       if (msg!=null){
+    	   try {
+			Message message = (Message)msg.getContentObject();
+			
+			switch(message.type){
+				case InitierAchat:
+				
+					break;
+					
+			}
+		} catch (UnreadableException e) {
+			e.printStackTrace();
+		}
+       }
+       block();
+    }
+}
