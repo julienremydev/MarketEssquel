7c907f34568b0cf08a8343f019a8db0aa8a2839e
Yodaii <yohann.vaubourg@gmail.com>
2016-12-06 16:45:03 +0100

    definition d'une période de prmomotion

--- src/main/java/agents/AgentGestion.java
+++ src/main/java/agents/AgentGestion.java
@@ -9,42 +9,67 @@ import fr.miage.agents.api.message.Message;
 import fr.miage.agents.api.message.TypeMessage;
 import fr.miage.agents.api.message.negociation.InitierAchat;
 import fr.miage.agents.api.message.recherche.Rechercher;
+import fr.miage.agents.api.message.util.PrevenirSolde;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 import jade.lang.acl.ACLMessage;
 import modele.Produit;
 import strategy.AchatProduit;
+import strategy.PrixVente;
+import strategy.Promotion;
 
 public class AgentGestion extends TickerBehaviour{
-	
+
 	public AgentGestion(Agent a){
 		super(a, 1000);
 	}
 
 	@Override
 	protected void onTick() {
-		
+		/*
+		 * Gestion des Stocks
+		 */
 		HashMap<Produit, Integer>  produitToBuy = AchatProduit.getWhatToBuy();
-		
+
 		Iterator it = produitToBuy.entrySet().iterator();
-	    while (it.hasNext()) {
-	        Map.Entry pair = (Map.Entry)it.next();
-	        ACLMessage achatProduit = new ACLMessage(ACLMessage.INFORM);
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry)it.next();
+			ACLMessage achatProduit = new ACLMessage(ACLMessage.INFORM);
 			achatProduit.addReceiver(new AID("AgentAchat", AID.ISLOCALNAME));
-			
+
 			Rechercher rech = new Rechercher();
 			rech.reference = ((Produit)pair.getKey()).getReference();
 			rech.prixMax = (Double)pair.getValue();
-			
+
 			achatProduit.setContentObject(rech);
 			this.getAgent().send(achatProduit);
-			
-	        it.remove(); // avoids a ConcurrentModificationException
-	    }
-	    
-		
-		
+
+			it.remove();
+		}
+
+
+		/*
+		 * Gestion des Prix
+		 */
+		PrixVente.updatePrice();
+
+
+		/*
+		 * Gestion des Soldes
+		 */
+		HashMap<String, Double> salesOrganize = Promotion.checkForSales();
+		if(salesOrganize != null){
+			ACLMessage sales = new ACLMessage(ACLMessage.INFORM);
+			sales.addReceiver(new AID("AgentVente", AID.ISLOCALNAME));
+
+			PrevenirSolde ps = new PrevenirSolde();
+			ps.categoriesSoldees = salesOrganize;
+
+			sales.setContentObject(ps);
+			this.getAgent().send(sales);
+		}
+
 		block();
 	}
 }
---
+++ src/main/java/strategy/Promotion.java
@@ -0,0 +1,121 @@
+package strategy;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import fr.miage.agents.api.message.recherche.Rechercher;
+import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+import jade.util.leap.ArrayList;
+import modele.Produit;
+import modele.Stock;
+import modele.SuperMarche;
+
+public class Promotion {
+
+	public static HashMap<String, Double> checkForSales() {
+		// TODO regarder les vieux stocks et la trésorerie pour établir une période de soldes de 2 jours dans 2 semaines
+		HashMap<String, Double> sales = new HashMap();
+		
+		/*
+		 * Calcul du ratio par rapport au capital du super marché
+		 */
+		SuperMarche market = SuperMarche.getSuperMarche("MarketEssquel");
+		float capital = market.getCapital();
+		double ratioCapital = calculRatioCapital(capital);
+		
+		/*
+		 * Calcul du ratio par rapport au vieux stock
+		 */
+		HashMap<String, Double> ratiosVieuxStocks = new HashMap();
+		String[] categories = {"cosmétique", "High-tech", "Produit d'entretien"};
+		for(int i=0; i<categories.length; i++){
+			List<Produit> produits = Produit.getProduits(categories[0]);
+			ratiosVieuxStocks.put(categories[0], calculRationStockCategorie(produits));
+		}
+		
+		
+		
+		Iterator it = ratiosVieuxStocks.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry)it.next();
+
+			pair.getKey();
+			double ratio = (double)pair.getValue()*ratioCapital;
+			
+			if(ratio > 0.40){
+				sales.put((String)pair.getKey(), 10.0);
+			}else if(ratio > 0.55){
+				sales.put((String)pair.getKey(), 20.0);
+			}else if(ratio > 0.70){
+				sales.put((String)pair.getKey(), 30.0);
+			}else if(ratio > 0.85){
+				sales.put((String)pair.getKey(), 40.0);
+			}
+
+			it.remove();
+		}
+			
+		return sales;
+	}
+	
+
+	public static double calculRatioCapital(float capital){
+		double ratio = 0;
+		
+		if(capital<500){
+			ratio = 1;
+		}else if(capital < 1000){
+			ratio = 0.75;
+		}else if(capital < 1500){
+			ratio = 0.5;
+		}else if(capital < 2000){
+			ratio = 0.25;
+		}
+		
+		return ratio;
+	}	
+	
+	private static Double calculRationStockCategorie(List<Produit> produits) {
+		double cumulRatio = 0;
+		int nbProduitsCategorie = produits.size();
+		
+		for(Produit produit : produits){
+			Stock stock = Stock.getVieuxStock(produit);
+			Date date = stock.getDateAchat();
+			int quantite = stock.getQuantite();
+			double ratioProduit = 1;
+			
+			long dateDiff = new Date().getTime() - date.getTime();
+			double nbJourDiff= dateDiff / (1000*60*60*24);
+			if(nbJourDiff > 90){
+				ratioProduit *= 0.95*(1-1/nbProduitsCategorie)*calculRatioQuantite(quantite);
+			}else if(nbJourDiff > 60){
+				ratioProduit *= 0.85*(1-1/nbProduitsCategorie)*calculRatioQuantite(quantite);
+			}else if(nbJourDiff > 30){
+				ratioProduit *= 0.75*(1-1/nbProduitsCategorie)*calculRatioQuantite(quantite);
+			}
+			
+			cumulRatio += ratioProduit;
+		}
+		
+		return cumulRatio/nbProduitsCategorie;
+	}
+
+	private static double calculRatioQuantite(int quantite) {
+		double ratio = 1;
+		
+		if(quantite > 100){
+			ratio = 0.95;
+		}else if(quantite > 75){
+			ratio = 0.85;
+		}else if(quantite > 50){
+			ratio = 0.75;
+		}
+		
+		return ratio;
+	}
+}
