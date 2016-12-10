a3b9bbb0c0e22e57575be83b90f8dd09662a939f
neo <arthurdu88@hotmail.com>
2016-12-10 15:41:41 +0100

    voilavoila julien a toi de jouer sur le mocker d'agent

---
+++ src/main/java/agents/Mocker.java
@@ -0,0 +1,17 @@
+package agents;
+
+import fr.miage.agents.api.message.Message;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+
+public class Mocker extends Agent 
+{
+
+    protected void setup() 
+    {
+        addBehaviour(new AgentK(this));
+    }
+}
+
