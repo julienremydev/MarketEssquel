a8bbf261fcc4411e970ec45f7f31ad61987f9c80
Julien REMY <Julien REMY>
2016-12-10 16:04:39 +0100

    ajout mock agent fournisseur

--- src/main/java/agents/AgentK.java
+++ src/main/java/agents/AgentK.java
@@ -1,33 +1,27 @@
 package agents;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.UUID;
 
 import fr.miage.agents.api.message.Message;
-import fr.miage.agents.api.message.negociation.AnnulerAchat;
 import fr.miage.agents.api.message.negociation.FinaliserAchat;
 import fr.miage.agents.api.message.negociation.InitierAchat;
-import fr.miage.agents.api.message.negociation.NegocierPrix;
-import fr.miage.agents.api.message.negociation.ResultatAnnulationAchat;
 import fr.miage.agents.api.message.negociation.ResultatFinalisationAchat;
 import fr.miage.agents.api.message.negociation.ResultatInitiationAchat;
-import fr.miage.agents.api.message.negociation.ResultatNegociation;
-import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
-import jade.lang.acl.UnreadableException;
-import strategy.AchatProduit;
 
 public class AgentK extends CyclicBehaviour{
 	private static final MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
 	public AgentK(Agent a){
 		super(a);
 	}
-	HashMap<UUID,ACLMessage> transaction = new HashMap<UUID,ACLMessage>();
-	HashMap<UUID,InitierAchat> produitEtQuantite = new HashMap<UUID,InitierAchat>();
+	HashMap<UUID,ArrayList<Message>> messages = new HashMap<UUID,ArrayList<Message>>();
+	Scanner sc = new Scanner(System.in);
 	public void action() 
 	{
 		block();
@@ -35,17 +29,50 @@ public class AgentK extends CyclicBehaviour{
 		if (msg!=null){
 			try {
 				Message message = (Message)msg.getContentObject();
-
+				ACLMessage response= new ACLMessage(ACLMessage.INFORM);
 				switch(message.type){
 				case InitierAchat:
-					// do your shit man
-					break;
+					InitierAchat ia = (InitierAchat)message;
 					
+					messages.put(ia.session, new ArrayList<Message>());
+					messages.get(ia.session).add(ia);
+					System.out.println("Entrez prix : ");
+					float prix = sc.nextFloat();
+					System.out.println("Entrez quantite: ");
+					int quantite = sc.nextInt();
+					System.out.println("Entrez boolean success (1 = true, 0 = false: ");
+					int isOk= sc.nextInt();
+					boolean success = false;
+					if (isOk==1) {
+						success = true;
+					}
+					ResultatInitiationAchat ria = new ResultatInitiationAchat();
+					ria.prixFixe=prix;
+					ria.quantiteDisponible=quantite;
+					ria.success=success;
+					ria.session=ia.session;
+					messages.get(ia.session).add(ria);
+					response.setContentObject(ria);
+					break;
+				case FinaliserAchat:
+					FinaliserAchat fa = (FinaliserAchat) message;
+					InitierAchat ia2 = (InitierAchat) messages.get(fa.session).get(0);
+					ResultatInitiationAchat ria2 = (ResultatInitiationAchat) messages.get(fa.session).get(0);
+					ResultatFinalisationAchat rfa = new ResultatFinalisationAchat();
+					rfa.session=fa.session;
+					rfa.idProduit=ia2.idProduit;
+					rfa.prixFinal=ria2.prixFixe;
+					rfa.quantiteProduit=ria2.quantiteDisponible;
+					response.setContentObject(rfa);
+					break;
+				default: 
+					System.out.println("Problème");
 				}
+				this.getAgent().send(response);
 			}
 			catch(Exception e)
 			{
-				
+				e.printStackTrace();
 			}
 		}
 	}
